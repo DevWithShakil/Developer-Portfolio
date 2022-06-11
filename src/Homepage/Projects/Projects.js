26604893cdd55dd1f2a000f0fbdf772a8f3e2371
Shakil Khan <m.shakilkhan720@gmail.com>
2022-06-11 20:49:37 +0600

    modal added with popup

--- src/Homepage/Projects/Projects.js
+++ src/Homepage/Projects/Projects.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import { CardGroup, Card } from 'react-bootstrap';
+import project1 from '../../assets/modal/project1.png'
 import project2 from '../../assets/logo/project2.png'
 import project3 from '../../assets/logo/project3.png'
 import project4 from '../../assets/logo/project4.png'
 import './project.css'
 
+
 const Projects = () => {
+
     return (
         <div class='bg-[#CDF0EA]'>
             <h2 class='text-4xl ml-10 mb-10 pt-10'>PROJECTS</h2>
@@ -18,20 +21,44 @@ const Projects = () => {
                         <div class="card-actions gap-10 pt-5">
                             <a href={"https://developer-portfolio-bf364.web.app/"} target="_blank" rel="noopener noreferrer" download className='btn btn-primary'> LIVE DEMO</a>
 
-                            <div>
-                                <label for="my-modal-6" class="btn modal-button btn-primary">DETAILS</label>
+                            {/* modal */}
 
-                                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
-                                <div class="modal modal-bottom sm:modal-middle">
-                                    <div class="modal-box">
-                                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
-                                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                                        <div class="modal-action">
-                                            <label for="my-modal-6" class="btn">Yay!</label>
-                                        </div>
+                            <label for="my-modal-5" class="btn modal-button btn-primary">DETAILS</label>
+
+                            <input type="checkbox" id="my-modal-5" class="modal-toggle" />
+                            <div class="modal">
+                                <div class="modal-box w-full h-full ">
+                                    <h2 class="card-title justify-center text-2xl font-bold uppercase mb-5">Developer Portfolio</h2>
+                                    <CardGroup class='flex'>
+                                        <Card>
+                                            <Card.Img variant="top" src={project1} />
+                                            <Card.Body>
+                                                <Card.Title>Features</Card.Title>
+                                                <Card.Text>
+                                                    <ul class='text-left'>
+                                                        <li>1. It's a Portfolio Website</li>
+                                                        <li>2. Using HTML5, Vanilla CSS</li>
+                                                        <li>3. Using Language JavaScript</li>
+                                                        <li>4. Using ReactJs</li>
+                                                    </ul>
+                                                </Card.Text>
+                                            </Card.Body>
+                                            <div class='flex justify-around'>
+                                                <div>
+                                                    <a href={"https://developer-portfolio-bf364.web.app/"} target="_blank" rel="noopener noreferrer" download className='btn btn-primary'> LIVE DEMO</a>
+                                                </div>
+                                                <div>
+                                                    <a href={"https://developer-portfolio-bf364.web.app/"} target="_blank" rel="noopener noreferrer" download className='btn btn-primary'>SOURCE CODE</a>
+                                                </div>
+                                            </div>
+                                        </Card>
+                                    </CardGroup>
+                                    <div class="modal-action">
+                                        <label for="my-modal-5" class="btn">Yay!</label>
                                     </div>
                                 </div>
                             </div>
+
 
                         </div>
                     </div>
